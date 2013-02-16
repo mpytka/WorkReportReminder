1254aae5bdf2e72b5854d108cbaebe9e2f05b66e
qlf00n <qlf00n@gmail.com>
2013-02-16 08:15:22 +0100

    [UT] adds unit tests project, adds first two unit tests for WorkItem class

--- UnitTests/Properties/AssemblyInfo.cs
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad34b754-176d-4243-a4a1-45337c9102dd")]
+[assembly: Guid("a883c6c6-6fa3-4570-b7a3-fcf2d0228773")]
 
 // Version information for an assembly consists of the following four values:
 //
