076046985da5eeb01e40d03570a046a444ae59cb
qlf00n <qlf00n@gmail.com>
2014-01-29 08:24:16 +0100

    added another few unit tests

---
+++ UnitTests/WorkItemDtoTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkReportReminder.Common;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WorkItemDtoTests
+    {
+        private const string COMMENT_CONTENT0 = "The first comment";
+        private const long WORK_ITEM_ID = 1;
+        private const string WORK_ITEM_TITLE = "Random Work Item Title";
+        private readonly DateTime m_commentDateTime0 = new DateTime(2013, 10, 20);
+        private readonly DateTime m_commentDateTime1 = new DateTime(2013, 10, 21);
+
+        [TestMethod]
+        public void ShouldInstantiateNewWorkItemDto()
+        {
+            // act
+            WorkItemDto workItemDto = new WorkItemDto(WORK_ITEM_ID, WORK_ITEM_TITLE, COMMENT_CONTENT0, m_commentDateTime0);
+
+            // assert
+            Assert.AreEqual(WORK_ITEM_ID, workItemDto.Id);
+            Assert.AreEqual(WORK_ITEM_TITLE, workItemDto.Title);
+            Assert.AreEqual(COMMENT_CONTENT0, workItemDto.Comment);
+            Assert.AreEqual(m_commentDateTime0, workItemDto.Time);
+           
+
+        }
+    }
+}
--- UnitTests/WorkItemsListTests.cs
+++ UnitTests/WorkItemsListTests.cs
@@ -8,8 +8,16 @@ namespace UnitTests
     [TestClass]
     public class WorkItemsListTests
     {
+        private const long WORK_ITEM_ID1 = 1;
+        private const long WORK_ITEM_ID2 = 2;
+        private const long WORK_ITEM_ID3 = 3;
+        private const string WORK_ITEM_TITLE1 = "First task";
+        private const string WORK_ITEM_TITLE2 = "Second task";
+        private const string WORK_ITEM_TITLE3 = "Third task";
+        private const string COMMENT_CONTENT0 = null;
+
         [TestMethod]
-        public void ShouldCollectionIsEmpty()
+        public void ShouldCollectionBeEmpty()
         {
             //arrange
             const int EXP_COUNT = 0;
@@ -117,6 +125,54 @@ namespace UnitTests
             Assert.AreEqual(1, result.Id);
             Assert.AreEqual("title" , result.Title);
             Assert.AreEqual(null, result.Comments);
+        }
+        [TestMethod]
+        public void ShouldReturnTwoCountedElements() // count method
+        {
+            //arrange
+            const int EXP_COUNT = 2;
+            WorkItem firstItem = new WorkItem(1, "title", null);
+            WorkItem secondItem = new WorkItem(2, "title", null);
+            WorkItemsList itemsList = new WorkItemsList();
+
+            //act
+            itemsList.Add(firstItem);
+            itemsList.Add(secondItem);
+
+            //assert
+            Assert.AreEqual(EXP_COUNT, itemsList.Count);
+        }
+        [TestMethod]
+        public void ShouldReturnLastElement() //LastWorkItem, Two items added
+        {
+            //arrange
+            WorkItem item = new WorkItem(1, "title", null);
+            WorkItem secondItem = new WorkItem(2, "newWI", null);
+            WorkItemsList itemsList = new WorkItemsList();
+            itemsList.Add(item);
+            itemsList.Add(secondItem);
+
+            //act
+            WorkItem result = itemsList.LastItem;
+           
+            //assert
+            Assert.AreEqual(2, result.Id);
+            Assert.AreEqual("newWI", result.Title);
+            Assert.AreEqual(null, result.Comments);
+        }
+        [TestMethod]
+        public void ShouldReturnEmptyWorkItemWhenNoItemsAdded() 
+        {
+            //arrange
+            WorkItemsList itemsList = new WorkItemsList();
+            
+            //act
+            WorkItem result = itemsList.LastItem;
+
+            //assert
+            Assert.AreEqual(0, result.Id);
+            Assert.AreEqual(string.Empty, result.Title);
+            Assert.AreEqual(1, result.Comments.Count);
         }
     }
 }
