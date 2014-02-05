a7a25271c0848e39bfd36c4f325a87ababef8484
qlf00n <qlf00n@gmail.com>
2014-02-05 09:20:34 +0100

    Added new test class and output file.

---
+++ UnitTests/DataManagerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WorkReportReminder.DataManagement;
+using WorkReportReminder.DataManagement.XmlData;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DataManagerTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"DataFiles\output1.xml")]
+        public void ShouldCreateDataManager()
+        {
+            IDataReader reader = new XmlDataReader();
+            IDataWriter writer = new XmlDataWriter(reader);
+            DataManagementConfiguration config = new DataManagementConfiguration(@"DataFiles\output1.xml");
+            DataManager manager = new DataManager(writer, reader,config);
+        }
+    }
+}
